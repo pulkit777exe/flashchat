ffa1105a82a7c854180c726a20c9f02a0ed7accc
pulkit777exe <pulkitpareek88@gmail.com>
2025-04-23 20:23:14 +0530

    feat: added chat and join logic

--- week_17_ws/dist/index.js
+++ week_17_ws/dist/index.js
@@ -2,17 +2,52 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const ws_1 = require("ws");
 const wss = new ws_1.WebSocketServer({ port: 8080 });
-let userCount = 0;
-let allSockets = [];
+console.log("WebSocket Server running on ws://localhost:8080");
+const allSockets = {};
 wss.on("connection", (socket) => {
-    allSockets.push(socket);
-    userCount = userCount + 1;
-    console.log("user connected #" + userCount);
-    socket.on("message", (message) => {
-        console.log("message received: " + message.toString());
-        for (let i = 0; i < allSockets.length; i++) {
-            const s = allSockets[i];
-            s.send(message.toString() + ": sent from the server");
+    console.log("User connected");
+    socket.on("message", (data) => {
+        let parsedMessage;
+        try {
+            parsedMessage = JSON.parse(data.toString());
+            const { type, message, roomId, personName } = parsedMessage;
+            if (type === "join") {
+                if (!allSockets[roomId]) {
+                    allSockets[roomId] = [];
+                }
+                allSockets[roomId].push(socket);
+                console.log(`User ${personName} joined room ${roomId}`);
+                socket.send(JSON.stringify({
+                    type: "info",
+                    message: `You joined room ${roomId}`,
+                }));
+            }
+            if (type === "chat" && message && allSockets[roomId]) {
+                console.log(`Message from ${personName} to room ${roomId}: ${message}`);
+                for (const client of allSockets[roomId]) {
+                    if (client.readyState === ws_1.WebSocket.OPEN) {
+                        client.send(JSON.stringify({
+                            type: "chat",
+                            message,
+                            roomId,
+                            personName
+                        }));
+                    }
+                }
+            }
+        }
+        catch (e) {
+            console.error("Failed to parse message", e);
+            socket.send(JSON.stringify({ type: "error", message: "Invalid format" }));
+        }
+    });
+    socket.on("close", () => {
+        console.log("User disconnected");
+        for (const roomId in allSockets) {
+            allSockets[roomId] = allSockets[roomId].filter((s) => s !== socket);
+            if (allSockets[roomId].length === 0) {
+                delete allSockets[roomId];
+            }
         }
     });
 });
--- week_17_ws/src/index.ts
+++ week_17_ws/src/index.ts
@@ -1,21 +1,94 @@
 import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
+console.log("WebSocket Server running on ws://localhost:8080");
 
-let userCount = 0;
-let allSockets: WebSocket[] = [];
+const allSockets: { [roomId: string]: WebSocket[] } = {};
 
-wss.on("connection", (socket) => {
-  allSockets.push(socket);
+/*
+{
+  "roomId": ["user1", "user2", ...]
+  "roomId1": ["user1", "user2", ...]
+  "roomId2": ["user1", "user2", ...]
+  }
+  
+  */
+ 
+ /*
+ {
+  "message" : "this is a message"
+  "type" : "chat" | "join"
+  "roomId" : Number
+  "personName" : String
+  }
+  
+  */
+ 
+ type MessageType = "chat" | "join";
 
-  userCount = userCount + 1;
-  console.log("user connected #" + userCount);
+interface MessagePayload {
+  type: MessageType;
+  message?: string;
+  roomId: string;
+  personName: string;
+}
 
-  socket.on("message", (message) => {
-    console.log("message received: " + message.toString());
-    for (let i = 0; i < allSockets.length; i ++) {
-      const s = allSockets[i];
-      s.send(message.toString() + ": sent from the server");
+wss.on("connection", (socket: WebSocket) => {
+  console.log("User connected");
+
+  socket.on("message", (data) => {
+    let parsedMessage:MessagePayload;
+
+    try {
+      parsedMessage = JSON.parse(data.toString());
+
+      const { type, message, roomId, personName } = parsedMessage;
+
+      if (type === "join") {
+        if (!allSockets[roomId]) {
+          allSockets[roomId] = [];
+        }
+        allSockets[roomId].push(socket);
+        console.log(`User ${personName} joined room ${roomId}`);
+
+        socket.send(
+          JSON.stringify({
+            type: "info",
+            message: `You joined room ${roomId}`,
+          })
+        );
+      }
+
+      if (type === "chat" && message && allSockets[roomId]) {
+        console.log(`Message from ${personName} to room ${roomId}: ${message}`);
+
+        for (const client of allSockets[roomId]) {
+          if (client.readyState === WebSocket.OPEN) {
+            client.send(
+              JSON.stringify({
+                type: "chat",
+                message,
+                roomId,
+                personName
+              })
+            );
+          }
+        }
+      }
+    } catch (e) {
+      console.error("Failed to parse message", e);
+      socket.send(JSON.stringify({ type: "error", message: "Invalid format" }));
+    }
+  });
+
+  socket.on("close", () => {
+    console.log("User disconnected");
+
+    for (const roomId in allSockets) {
+      allSockets[roomId] = allSockets[roomId].filter((s) => s !== socket);
+      if (allSockets[roomId].length === 0) {
+        delete allSockets[roomId];
+      }
     }
   });
 });
