91bd31e2b8adaa06203086db53d5e3a5d5606901
pulkit777exe <pulkitpareek88@gmail.com>
2025-07-29 01:09:37 +0530

    changed websocket connnection contructor

--- flash_fe/src/components/ContainerSection.tsx
+++ flash_fe/src/components/ContainerSection.tsx
@@ -28,7 +28,7 @@ export const ContainerSection = () => {
 
   useEffect(() => {
     // Determine the WebSocket URL. Use environment variable or default to localhost.
-    const ws = new WebSocket(import.meta.env.REACT_APP_WEBSOCKET_URL || "ws://localhost:8080");
+    const ws = new WebSocket(import.meta.env.REACT_APP_WEBSOCKET_URL+":"+import.meta.env.REACT_APP_WEBSOCKET_PORT);
 
     // Event listener for when the WebSocket connection is opened.
     ws.onopen = () => {
